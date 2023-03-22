7fa7d11ac4348d658ece99434732a207b3fd837c
zhangfeng <zhangfeng@feeling-ai.cn>
2023-03-22 19:45:22 +0800

    add vpc and tke

---
+++ terraform/modules/tkeandvpc/cluster/outputs.tf
@@ -0,0 +1,24 @@
+output "id" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.id
+}
+output "kube_config" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.kube_config
+}
+output "kube_config_intranet" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.kube_config_intranet
+}
+output "certification_authority" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.certification_authority
+}
+output "user_name" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.user_name
+}
+output "cluster_external_endpoint" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.cluster_external_endpoint
+}
+output "pgw_endpoint" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.pgw_endpoint
+}
+output "security_policy" {
+  value = tencentcloud_kubernetes_cluster.k8s_cluster.security_policy
+}
---
+++ terraform/modules/tkeandvpc/cluster/variables.tf
@@ -0,0 +1,83 @@
+variable "vpc_name" {
+  description = "The vpc name used to launch a new vpc when 'vpc_id' is not specified."
+  default     = "tf-modules-vpc"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The cidr block used to launch a new vpc when 'vpc_id' is not specified."
+  default     = "172.16.0.0/16"
+  type        = string
+}
+
+variable "vpc_is_multicast" {
+  description = "Specify the vpc is multicast when 'vpc_id' is not specified."
+  default     = true
+  type        = bool
+}
+
+variable "vpc_dns_servers" {
+  description = "Specify the vpc dns servers when 'vpc_id' is not specified."
+  type        = list(string)
+  default     = []
+}
+
+variable "vpc_tags" {
+  description = "Additional tags for the vpc."
+  type        = map(string)
+  default     = {}
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  default = "ap-beijing-1"
+  type = string
+}
+
+variable "name" {
+  default = "default-cluster"
+}
+
+variable "k8s_version" {
+  default = "1.20.6"
+}
+
+variable "cluster_cidr" {
+  # cidr for pod/service etc
+  # ip for node is within the vpc
+  # max pod per node 256
+  # max services 2048
+  # max nodes 248
+  default = "192.168.0.0/16"
+}
+
+variable "default_instance_type" {
+  default = "SA3.MEDIUM8"
+}
+
+variable "name" {
+  description = "Subnet name"
+  default = "guagua-ci-temp-test"
+  type = string
+}
+
+variable "cidr_block" {
+  description = "Subnet CIDR block"
+  default = "10.0.20.0/28"
+  type = string
+}
+
+variable "is_multicast" {
+  description = "Subnet is multicast"
+  default = false
+  type = bool
+}
+
+variable "password" { 
+  # TODO: actual password from ssm
+  default = "Default@pass1"
+}
+
+variable "az" {
+  default = "ap-beijing-6"
+}
